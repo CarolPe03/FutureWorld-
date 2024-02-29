9a909d249f3d61f1b5551971ef6e62ce4e6a9cc0
CarolPe03 <anacarolpc11@gmail.com>
2024-02-28 23:47:02 -0600

    2.5 Optimizacion de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,10 +1,19 @@
+import styles from './Description.module.sass'
+import Image from 'next/image';
+
 export const Description = () => {
     return (
-        <section>
-            <h1>Descripción</h1>
-            <img src="/images/description.jpeg" alt="products marketplace" />
-            <h2>Description</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error dolore voluptate, animi aperiam dicta pariatur sequi neque vel sit incidunt commodi est molestiae ullam veritatis asperiores consequatur! Autem, pariatur beatae.</p>
+        <section className={styles.Description}>
+            <Image
+                src="/images/description.jpeg"
+                alt="products marketplace"
+                width={500}
+                height={300}
+            />
+            <div className={styles.Description__text}>
+                <h2>Bring the future today</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error dolore voluptate, animi aperiam dicta pariatur sequi neque vel sit incidunt commodi est molestiae ullam veritatis asperiores consequatur! Autem, pariatur beatae.</p>
+            </div>
         </section>
     )
 }
